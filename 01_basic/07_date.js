0d4f382f6427d5daec11fd716a0af72b927c19d1
manishh101 <125335510+manishh101@users.noreply.github.com>
2023-11-26 12:18:45 +0000

    date and time

---
+++ 01_basic/07_date.js
@@ -0,0 +1,13 @@
+//Dates
+
+let myDate = new Date()
+//console.log(myDate);
+//console.log(myDate.toString());
+//console.log(myDate.toDateString());
+//console.log(myDate.toISOString());
+//console.log(myDate.toJSON());
+//console.log(myDate.toLocaleDateString());
+
+//let myCreatedDate = new Date (2023,0,24)
+let myCreatedDate = new Date (2023,0,24,5,42)
+console.log(myCreatedDate.toLocaleString());
