ed6a417cbd17ce320bb127ce2681577d2defb5ff
manishh101 <125335510+manishh101@users.noreply.github.com>
2023-11-26 11:15:55 +0000

    maths and num

---
+++ 01_basic/06_nums_and_maths.js
@@ -0,0 +1,19 @@
+const score = 200
+//console.log(typeof (score));
+
+const balance = new Number (100)
+//console.log(typeof(balance));
+
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(2));
+
+const otherNumber = 123.89760
+//console.log(otherNumber.toPrecision(4));
+
+const hundreds =100000000
+//console.log(hundreds.toLocaleString('en-in'));
+
+
+//*******************Maths********************** */
+
+console.log(Math);
