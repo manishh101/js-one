df48dbc8ae1768f10bc3f2f317976ccf5683a356
manishh101 <125335510+manishh101@users.noreply.github.com>
2023-11-27 00:04:21 +0000

    arrays

--- 01_basic/07_date.js
+++ 01_basic/07_date.js
@@ -10,4 +10,29 @@ let myDate = new Date()
 
 //let myCreatedDate = new Date (2023,0,24)
 let myCreatedDate = new Date (2023,0,24,5,42)
-console.log(myCreatedDate.toLocaleString());
+//console.log(myCreatedDate.toLocaleString());
+
+let currentDate = new Date();
+
+let year = currentDate.getFullYear();
+let month = currentDate.getMonth(); // Month is zero-based (0 = January, 11 = December)
+let day = currentDate.getDate();
+let hours = currentDate.getHours();
+let minutes = currentDate.getMinutes();
+let seconds = currentDate.getSeconds();
+let milliseconds = currentDate.getMilliseconds();
+
+//console.log(`${year}-${month + 1}-${day + 1} ${hours}:${minutes}:${seconds}`);
+
+//Formatting Dates
+
+console.log(currentDate.toLocaleString()); // Default format based on user's locale
+
+// Using moment.js for more formatting options
+// Example: npm install moment
+const moment = require('moment');
+console.log(moment(currentDate).format('MMMM Do YYYY, h:mm:ss a'));
+
+
+
+
