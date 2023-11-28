7ebd50f14c1c823e3efdfdbdafae6b521ee2b202
manishh101 <125335510+manishh101@users.noreply.github.com>
2023-11-28 01:25:42 +0000

    arrays

--- 02_basic/02_array.js
+++ 02_basic/02_array.js
@@ -1,7 +1,22 @@
-const marvelHeros = ["thor","tronman","spiderman"]
+const marvelHeros = ["thor","ironman","spiderman"]
 const dcHeros = ["superman","flash","batman"]
 
-marvelHeros.push(dcHeros)
+//marvelHeros.push(dcHeros)
 
-console.log(marvelHeros);
+//console.log(marvelHeros);
 
+// allHeros = marvelHeros.concat(dcHeros)
+// console.log(allHeros);
+
+const allHeros = [...marvelHeros,...dcHeros];
+//console.log(allHeros);
+
+const otherArray = [1,2,3,[4,5,[7,8,9]]];
+const realArray = otherArray.flat(3) 
+//console.log(realArray);
+
+let num1 = 10
+let num2 = 20
+let num3 = 30
+
+console.log(Array.of(num1,num2,num3));
