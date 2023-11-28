05cc416e4b09b0efa79956128010a9e53b88b4e1
manishh101 <125335510+manishh101@users.noreply.github.com>
2023-11-28 02:25:39 +0000

    object

--- 02_basic/03_object.js
+++ 02_basic/03_object.js
@@ -1,1 +1,42 @@
 //singleton
+//Object.create
+
+//object literals
+
+const mySym = Symbol("key2");
+
+const JsUser = {
+    name : 'Manish',
+    "full_name": 'Manish Rajbanshi',
+    age : 20,
+    [mySym] : "sym",
+    level : 'bachelor',
+    location : "ktm",
+    email : "manishhrajbanshi@gmail.com",
+    isLoggedIn : false,
+    lastLoginDays : ['sunday','monday']
+
+}
+
+// console.log(JsUser.name);
+// console.log(JsUser['email']);
+// console.log(JsUser.full_name);
+// console.log(typeof(JsUser[mySym]));
+
+//JsUser.email = "rajbanshibbs@gmail.com";
+// console.log(JsUser['email']);
+//Object.freeze(JsUser)
+//JsUser.email = "manishhibbs@gmail.com";
+// console.log(JsUser);
+
+JsUser.greeting = function() {
+    console.log('Hello js user');
+}
+
+JsUser.greetingTwo = function() {
+    console.log(`hello Js User, ${this.name}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
+
