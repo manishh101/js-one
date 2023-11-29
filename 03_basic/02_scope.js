40fe73d8c255c1acd56684cb42776e5c5c952169
manishh101 <125335510+manishh101@users.noreply.github.com>
2023-11-29 03:16:14 +0000

    scope

---
+++ 03_basic/02_scope.js
@@ -0,0 +1,35 @@
+function one(){
+    const username= 'manish'
+    function two (){
+        const website = "github"
+        //console.log(username);
+    }
+    //console.log(website);
+
+    two()
+}
+one()
+
+if (true){
+    const username='manish'
+    if(username==='manish'){
+        const website = 'github'
+        //console.log(username + website);
+    }
+    //console.log(website);
+}
+//console.log(username);
+
+//+++++++++++++++ interesring ++++++++++++++++++//
+console.log(addOne(5));
+
+function addOne(num){
+   return num + 2 
+}
+
+console.log(addTwo(5));
+
+const addTwo = function(num){
+    return num + 2
+}
+
