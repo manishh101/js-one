3436b721d5406c68e2bbd99cf62b796088f46a07
manishh101 <125335510+manishh101@users.noreply.github.com>
2023-11-29 11:56:09 +0000

    three

---
+++ 04_basic/three.js
@@ -0,0 +1,28 @@
+
+const heros = ['superman','batman','ironman']
+
+// for (const num1 of heros) {
+//   console.log(num1);  
+// }
+
+// const greetings = 'hello wolrd'
+// for (const greet of greetings) {
+//     console.log(`each char is ${greet}`);
+// }
+
+// for (const key in heros) {
+//     if (heros.hasOwnProperty.call(heros, key)) {
+//         const element = heros[key];
+//         console.log(element);
+        
+//     }
+// }
+
+//Maps
+
+const map = new Map()
+map.set('IN',"India")
+map.set("USA","United States of America")
+map.set("Fr","France")
+
+console.log(map);
