317414828e80fcefb04593c4cf6a585fe27a4dd2
manishh101 <125335510+manishh101@users.noreply.github.com>
2023-11-30 01:17:39 +0000

    six

--- 04_basic/five.js
+++
@@ -1,9 +0,0 @@
-const coding = ['js','ruby','java','python','cpp']
-
-// coding.forEach(function(val){
-//     console.log(val);
-// })
-
-coding.forEach( (value) =>  {
-console.log(value);
-})
---
+++ 05_iterations/six.js
@@ -0,0 +1,13 @@
+const coding = ['js','ruby','java','python','cpp']
+
+// coding.forEach(function(val){
+//     console.log(val);
+// })
+
+// coding.forEach( (value) =>  {
+// console.log(value);
+// })
+
+const myNums = [1,2,3,4,5,6,7,8,9,10]
+
+console.log(myNums.filter((num) => num>4));
